0f5ef466079bbffe85066db7b9b08a6e0216ad30
Abdul Qadeer <abdul@cidrusmoney.com>
2023-11-27 21:39:43 +0500

    added register anchor link in the login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,5 +43,14 @@
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+
+        <div class="flex items-center justify-end mt-4">
+        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+                {{ __("Don't Have any account yet ?") }}
+            </a>
+
+        </div>
+
+
     </form>
 </x-guest-layout>
